0b52ad598436e338439ce6fe8bebb62e42fbe8fc
InaamR <83518345+InaamR@users.noreply.github.com>
2022-05-26 11:20:28 +0100

    config

--- config/config.php
+++ config/config.php
@@ -18,17 +18,17 @@ $PARAM_mot_passe   = '';
 $PARAM_nom_site    = 'Expert-Gaming'; 
 $PARAM_mail_site   = 'contact@expert-gaming.tn'; 
 $PARAM_url_site    = 'http://localhost/version_enligne_front/'; 
-$PARAM_url_non_doc_site    = 'version_enligne_front/'; 
+$PARAM_url_non_doc_site    = 'version_enligne_front/';
 
 
-/*$PARAM_hote        = 'localhost'; 
+/*$PARAM_hote        = '196.203.63.37'; 
 $PARAM_nom_bd      = 'expertga_expert_gaming'; 
 $PARAM_utilisateur = 'expertga_admin'; 
 $PARAM_mot_passe   = 'Babnet@2020209999'; 
 $PARAM_port   = '3306';
 $PARAM_nom_site    = 'Expert-Gaming'; 
 $PARAM_mail_site   = 'contact@expert-gaming.tn';  
-$PARAM_url_site    = 'https://betatestfront.expert-gaming.tn/'; 
+$PARAM_url_site    = 'https://test.expert-gaming.tn/'; 
 $PARAM_url_non_doc_site    = '';*/
 
 ?>
